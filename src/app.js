5ed2592d3a49c21487dbb3db50e32c6df5cc05b4
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2023-04-06 17:03:04 -0300

    Update Post tweet, now using header
    Atualização do método post de tweet, agora usando o header para capturar o username

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = 5000;
 app.use(cors());
 app.use(express.json());
+let user = {};
 let users = [];
 let tweetList = [];
 
@@ -12,6 +13,7 @@ app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
 
 app.post("/sign-up", (req, res) => {
     const {username, avatar} = req.body;
+    user = req.body;
     if(((typeof(username) !== 'string') || username.length === 0) || ((typeof(avatar) !== 'string') || avatar.length === 0)){
         res.status(400).send("Todos os campos são obrigatórios!");
         return
@@ -21,8 +23,9 @@ app.post("/sign-up", (req, res) => {
 })
 
 app.post("/tweets", (req, res) => {
-    const  {username, tweet} = req.body;
-    if(((typeof(username) !== 'string') || username.length === 0) || ((typeof(tweet) !== 'string') || tweet.length === 0)){
+    const  {tweet} = req.body;
+    const username = req.headers.user;
+    if(((typeof(username) !== 'string') || user.username.length === 0) || ((typeof(tweet) !== 'string') || tweet.length === 0)){
         res.status(400).send("Todos os campos são obrigatórios!");
         return
     }
