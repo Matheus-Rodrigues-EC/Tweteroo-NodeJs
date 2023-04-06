31a4d87ef5d1ea0f3ecd26200e29d83102e84ceb
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2023-04-06 15:28:34 -0300

    Add validations and confirm configurations of status codes
    Adicionada a validação de cada rota de post e confirmada a configuração de cada status code

--- src/app.js
+++ src/app.js
@@ -1,28 +1,35 @@
 import express from "express";
 import cors from "cors";
 import chalk from "chalk";
+import { send } from "process";
 
 const app = express();
 const PORT = 5000;
 app.use(cors());
 app.use(express.json());
-let user = {};
 let users = [];
 let tweetList = [];
 
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
 
 app.post("/sign-up", (req, res) => {
-    user = req.body;
-    if(!users.find((profile) => profile.username === user.username)) users.push(user);
+    const {username, avatar} = req.body;
+    if(((typeof(username) !== 'string') || username.length === 0) || ((typeof(avatar) !== 'string') || avatar.length === 0)){
+        res.status(400).send("Todos os campos são obrigatórios!");
+        return
+    }
+    if(!users.find((profile) => profile.username === username)) users.push({username, avatar});
     res.status(201).send("OK");
 })
 
 app.post("/tweets", (req, res) => {
-    let user = req.body.username;
-    if(users.find((profile) => profile.username === user)){
-        const tweet = req.body;
-        tweetList.push(tweet);
+    const  {username, tweet} = req.body;
+    if(((typeof(username) !== 'string') || username.length === 0) || ((typeof(tweet) !== 'string') || tweet.length === 0)){
+        res.status(400).send("Todos os campos são obrigatórios!");
+        return
+    }
+    if(users.find((profile) => profile.username === username)){
+        tweetList.push({username, tweet});
         res.status(201).send("OK");    
     }else{
         res.status(401).send("UNAUTHORIZED");
